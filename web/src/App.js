110e520c4c53ffabf4504c22ce760176c9b268d1
Dants0 <gui.dg1@hotmail.com>
2022-11-08 18:50:22 -0300

    fix: new features and a little hard feature

--- web/src/App.js
+++ web/src/App.js
@@ -11,14 +11,18 @@ import "./style.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/authContext";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
   const { currentUser } = useContext(AuthContext);
 
   const { darkMode } = useContext(DarkModeContext);
 
+  const queryClient = new QueryClient()
+
   const Layout = () => {
     return (
+      <QueryClientProvider client={queryClient}>
       <div className={`theme-${darkMode ? "dark" : "light"}`}>
         <Navbar />
         <div style={{ display: "flex" }}>
@@ -29,6 +33,7 @@ function App() {
           <RightBar />
         </div>
       </div>
+  </QueryClientProvider>
     );
   };
 
@@ -36,7 +41,7 @@ function App() {
     if (!currentUser) {
       return <Navigate to="/login" />;
     }
-
+    
     return children;
   };
 
